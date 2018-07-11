af6be70402020b6f94236b6c02720ef0c4b8421b
channa <charles.senevi@theloop.com.au>
2018-07-11 21:52:48 +0530

    setup partials

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <app-header></app-header>
+      <router-view></router-view>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+import header from './views/Partials/Header.vue'
+import footer from './views/Partials/Footer.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'app-header': header,
+    'app-footer': footer
+  }
 }
 </script>
 
